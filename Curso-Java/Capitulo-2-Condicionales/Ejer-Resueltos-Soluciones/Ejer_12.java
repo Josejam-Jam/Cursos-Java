2e0df973a41eb9069f481f7bc2951e9258245bae
josejam <joseam.jam.79@gmail.com>
2024-09-26 14:13:59 +0200

    add mod Ejer-12-V4 Ejercicios-Condicionales-Capitulo-2

--- Curso-Java/Capitulo-2-Condicionales/Ejer-Resueltos-Soluciones/Ejer_12.java
+++ Curso-Java/Capitulo-2-Condicionales/Ejer-Resueltos-Soluciones/Ejer_12.java
@@ -44,15 +44,15 @@ public class Ejer_12 {
                 
             } else {
                 
-                if(mes == 2 && (1 <= dia && dia <= 28)) {
+                if(mes == 2 && (1 >= dia && dia <= 28)) {
                     
                     fechaCorrecta = true;
                     
-                } else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (1 <= dia && dia >= 30)) {
+                } else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (1 >= dia && dia >= 30)) {
                     
                     fechaCorrecta = true;
                     
-                } else if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (1 <= dia && dia <= 31)) {
+                } else if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (1 >= dia && dia <= 31)) {
                     
                     fechaCorrecta = true;
                     
